824b8c40cd7208942917a3b82d017f9b4372c548
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2020-11-06 20:57:18 +0100

    Initial commit and main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // public GameObject player;
+    // private GameObject clonePlayer;
+    private GameObject currentFrame;
+    private GameObject nextFrame;
+
+    // void OnEnable()
+    // {
+    //     SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    // }
+
+    // void OnDisable()
+    // {
+    //     SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    // }
+
+    // void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    // {
+    //     if (scene.buildIndex == 1)
+    //     {
+    //          clonePlayer = Instantiate(player, new Vector3(0f, 12.5f, 0f), Quaternion.identity) as GameObject;
+    //     }
+    // }
+
+    public void PlayGame() 
+    {
+        // FindObjectOfType<AudioManager>().IsPlaying("Theme");
+        FindObjectOfType<LevelLoader>().LoadNextScene("Game");
+    }
+
+    public void Retry()
+    {
+        // Destroy(clonePlayer);
+        // FindObjectOfType<LevelLoader>().LoadNextScene("Game");
+        // clonePlayer = Instantiate(player, new Vector3(0f, 12.5f, 0f), Quaternion.identity) as GameObject;
+    }
+
+    public void GoToMenu()
+    {
+        FindObjectOfType<LevelLoader>().LoadNextScene("Menu");
+    }
+
+    public void GameOver(GameObject currentFrame, GameObject nextFrame)
+    {
+        // currentFrame = currentFrame;
+        // nextFrame = nextFrame;
+        // if (currentFrame.active == true)
+        //         {
+        //             currentFrame.SetActive(false);
+        //             nextFrame.SetActive(true);
+        //             Destroy(clonePlayer);
+        //         }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void WinGame(GameObject currentFrame, GameObject nextFrame)
+    {
+        // currentFrame = currentFrame;
+        // nextFrame = nextFrame;
+        // if (currentFrame.active == true)
+        //         {
+        //             currentFrame.SetActive(false);
+        //             nextFrame.SetActive(true);
+        //             Destroy(clonePlayer);
+        //         }
+    }
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f; 
+
+    public void LoadNextScene(string name)
+    {
+        StartCoroutine(LoadLevel(name));
+    }
+
+    IEnumerator LoadLevel(string name)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(name);
+    }
+}
