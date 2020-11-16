b2f21a0d4efbdc2ad1f62c7cfe987dc77796b8d7
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2020-11-16 09:53:52 -0500

    start working on randomly assigning roles to players

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,75 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
 
 public class GameManager : MonoBehaviour
 {
-    // public GameObject player;
-    // private GameObject clonePlayer;
-    private GameObject currentFrame;
-    private GameObject nextFrame;
+    private int index; 
+    public List<GameObject> characterList = new List<GameObject>();
 
-    // void OnEnable()
-    // {
-    //     SceneManager.sceneLoaded += OnLevelFinishedLoading;
-    // }
-
-    // void OnDisable()
-    // {
-    //     SceneManager.sceneLoaded -= OnLevelFinishedLoading;
-    // }
-
-    // void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
-    // {
-    //     if (scene.buildIndex == 1)
-    //     {
-    //          clonePlayer = Instantiate(player, new Vector3(0f, 12.5f, 0f), Quaternion.identity) as GameObject;
-    //     }
-    // }
-
-    public void PlayGame() 
-    {
-        // FindObjectOfType<AudioManager>().IsPlaying("Theme");
-        FindObjectOfType<LevelLoader>().LoadNextScene("Game");
-    }
-
-    public void Retry()
-    {
-        // Destroy(clonePlayer);
-        // FindObjectOfType<LevelLoader>().LoadNextScene("Game");
-        // clonePlayer = Instantiate(player, new Vector3(0f, 12.5f, 0f), Quaternion.identity) as GameObject;
-    }
-
-    public void GoToMenu()
-    {
-        FindObjectOfType<LevelLoader>().LoadNextScene("Menu");
-    }
-
-    public void GameOver(GameObject currentFrame, GameObject nextFrame)
-    {
-        // currentFrame = currentFrame;
-        // nextFrame = nextFrame;
-        // if (currentFrame.active == true)
-        //         {
-        //             currentFrame.SetActive(false);
-        //             nextFrame.SetActive(true);
-        //             Destroy(clonePlayer);
-        //         }
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
-    public void WinGame(GameObject currentFrame, GameObject nextFrame)
-    {
-        // currentFrame = currentFrame;
-        // nextFrame = nextFrame;
-        // if (currentFrame.active == true)
-        //         {
-        //             currentFrame.SetActive(false);
-        //             nextFrame.SetActive(true);
-        //             Destroy(clonePlayer);
-        //         }
-    }
 }
--- Assets/Scripts/NetworkGamePlayerAvalon.cs
+++ Assets/Scripts/NetworkGamePlayerAvalon.cs
@@ -3,41 +3,37 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+ 
 
 
 public class NetworkGamePlayerAvalon : NetworkBehaviour
 {
+
+    private int index; 
+
+
+    void Start()
+     {
+
+        GameObject GameManager = GameObject.Find("GameManager");
+        GameManager gameManager = GameManager.GetComponent<GameManager>();
+        index = UnityEngine.Random.Range(0,gameManager.characterList.Count-1);
+        Instantiate(gameManager.characterList[index], transform);
+        gameManager.characterList.RemoveAt(index);
+        Debug.Log(gameManager.characterList);
+
+     }
+
   
     [SyncVar]
     private string displayName = "Loading...";
-
-    // private NetworkManagerAvalon room;
-    // private NetworkManagerAvalon Room
-    // {
-    //     get
-    //     {
-    //         if (room != null) { return room; }
-    //         return room = NetworkManager.singleton as NetworkManagerAvalon;
-    //     }
-    // }
-
-    // public override void OnStartClient()
-    // {
-    //     DontDestroyOnLoad(gameObject);
-    //     Room.GamePlayers.Add(this);
-
-    // }
-
-    // public override void OnStopClient()
-    // {
-        
-    //     Room.GamePlayers.Remove(this);
-
-    // }
 
     [Server]
     public void SetDisplayName(string displayName)
     {
         this.displayName = displayName;
     }
+
+
 }
