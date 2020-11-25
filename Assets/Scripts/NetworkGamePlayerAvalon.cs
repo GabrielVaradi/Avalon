b5c313d93062c275eb480d73b16a16f11118f734
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2020-11-25 11:06:27 -0500

    delete commented code

--- Assets/Scripts/NetworkGamePlayerAvalon.cs
+++ Assets/Scripts/NetworkGamePlayerAvalon.cs
@@ -35,26 +35,7 @@ public class NetworkGamePlayerAvalon : NetworkBehaviour
                 Instantiate(board.hiddenPlayer, new Vector3(spawnPoint.transform.position.x, spawnPoint.transform.position.y, -1), spawnPoint.transform.rotation);
             }
         }
-
-        // GameObject GameManager = GameObject.Find("GameManager");
-
-        // GameManager gameManager = GameManager.GetComponent<GameManager>();
-
-        // index = UnityEngine.Random.Range(0,gameManager.characterList.Count-1);
-
-        // Instantiate(gameManager.characterList[index], transform);
-
-        // gameManager.characterList.RemoveAt(index);
-
-        // playerPosition = GameObject.FindGameObjectWithTag("NetworkGamePlayer").transform.position;
-
-        // foreach (GameObject spawnPoint in gameManager.spawnPoints)
-        // {
-        //     if (spawnPoint.transform.position != playerPosition)
-        //     {
-        //         Instantiate(gameManager.hiddenPlayer, spawnPoint.transform.position, spawnPoint.transform.rotation);
-        //     }
-        // }
+        
      }
 
   
