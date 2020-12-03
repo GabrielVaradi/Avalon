72cda36e87742124fe0f32deced52c4fa41fe60f
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2020-12-03 14:57:46 -0500

    idem

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,5 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    // private int index; 
-    // public List<GameObject> characterList = new List<GameObject>();
-    // public List<GameObject> spawnPoints = new List<GameObject>();
-    // public GameObject hiddenPlayer; 
 
 }
